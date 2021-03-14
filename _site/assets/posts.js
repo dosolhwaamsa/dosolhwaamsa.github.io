6440e74d504889e57ce74dcb2cb6958da0e9b5fd
dosolhwaamsa <dosol-hwaam@naver.com>
2021-03-15 05:10:28 +0900

    .

--- _site/assets/posts.js
+++ _site/assets/posts.js
@@ -1480,46 +1480,41 @@ posts_json=[{
     "content":"Use technology to make an impact, inspire change. We have programmes for citizens, businesses and government agencies to get on board. Be involved now. Co-creation In an inter-connected world where data is open and available to all, citizens and businesses can do a lot to shape how Singapore develops into a Smart Nation. Indeed, it is through your engagement and co-creation that a Smart Nation becomes truly smart. Today, there are several resources to get involved in a digital Singapore. By discovering new insights, sharing feedback and developing apps, many ground-up efforts of users are already shaping Singapore for the future. Tech Kaki Community We want to solve problems. In fact, we want to solve problems with you! At GovTech Singapore, we transform the delivery of Government digital services by taking an “outside-in” view, and put users first for our work. Tech Kaki is a community set up by GovTech for citizen participation. As part of this community, you will get first dibs on new tech products before they go live and be part of the development team to make them better. We will publish quick polls and mini surveys regularly. All you have to do is to take a few minutes to answer them. Join us at https://www.facebook.com/TechKaki today! Bug Bounty Program At GovTech, we embrace this “outside-in approach”. Only when we have taken in the views and preferences of citizens and stakeholders can a digital service be considered user-centric. This, we believe, is the way to develop high-quality digital services that matter. This is also why we are asking people to be our Tech Kaki. By joining the community, you get to try out our new tech products and services before they are available. Too many government apps today? That’s alright, we will evaluate the landscape of apps as we move towards addressing the various Moments of Life for citizens. As a user, your insights are valuable in creating digital offerings that are useful to citizens.",
 },{
     "id": "296",
-    "url": "/resource_room/volunteer/",
-    "title": "참여마당",
-    "content":"",
-},{
-    "id": "297",
     "url": "/resource_room/notices-hwaam/",
     "title": "참여마당",
     "content":"",
 },{
-    "id": "298",
+    "id": "297",
     "url": "/resource_room/references/",
     "title": "참여마당",
     "content":"",
 },{
-    "id": "299",
+    "id": "298",
     "url": "/resource_room/religious-life/",
     "title": "참여마당",
     "content":"",
 },{
-    "id": "300",
-    "url": "/resource_room/notices-dosolnoin/",
-    "title": "참여마당",
-    "content":"",
-},{
-    "id": "301",
-    "url": "/resource_room/forest/",
-    "title": "참여마당",
-    "content":"",
-},{
-    "id": "302",
+    "id": "299",
     "url": "/resource_room/",
     "title": "참여마당",
     "content":"",
 },{
-    "id": "303",
+    "id": "300",
+    "url": "/resource_room/forest/",
+    "title": "참여마당",
+    "content":"",
+},{
+    "id": "301",
+    "url": "/resource_room/notices-dosolnoin/",
+    "title": "참여마당",
+    "content":"",
+},{
+    "id": "302",
     "url": "/",
     "title": "도솔산화암사",
     "content":"홈페이지 리뉴얼중입니다. 최신공지는 다음 링크에서 확인해주세요. [링크바로가기]",
 },{
-    "id": "304",
+    "id": "303",
     "url": "/search/",
     "title": "Search",
     "content":"",
